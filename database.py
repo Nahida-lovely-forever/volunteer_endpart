070c3e92cdcf6880fa825b622390caefabeca58a
hailillya <hailillya1@gmail.com>
2025-08-27 10:34:02 +0800

    8.27.2

--- database.py
+++ database.py
@@ -1,2 +1,10 @@
 import pandas as pd
 import numpy as np
+
+project_database= pd.DataFrame(
+    columns=['id','pname','description','type',
+             'time','reward','required_personal',
+             'maxnum','contact_name','contact_id',
+             'contact_phone']
+    )
+project_database.to_csv('project_database.csv', index=False)
